8d4f1885569590285fcdc2f58d6e16be9879f8d3
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-03-24 20:35:12 -0400

    Get the port of the running instance

--- cambio-service/src/main/java/ca/java/cambio/controller/CambioController.java
+++ cambio-service/src/main/java/ca/java/cambio/controller/CambioController.java
@@ -2,6 +2,8 @@ package ca.java.cambio.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,13 @@ import ca.java.cambio.model.Cambio;
 @RestController
 @RequestMapping("/cambio-service")
 public class CambioController {
+	@Autowired
+	private Environment env;
+
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from,
 			@PathVariable("to") String to) {
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "8000");
+		var port = env.getProperty("local.server.port");
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
